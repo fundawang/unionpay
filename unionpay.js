750003219952d1be081df5f0506deaeafad98fe3
Funda Wang <fundawang@gmail.com>
2012-12-19 18:35:16 +0800

    set correct length of security_key

--- unionpay.js
+++ unionpay.js
@@ -1,20 +1,11 @@
-function password(length, special) {
+function password(length) {
   var iteration = 0;
   var password = "";
   var randomNumber;
-  if(special == undefined){
-      var special = false;
-  }
   while(iteration < length){
-    randomNumber = (Math.floor((Math.random() * 100)) % 94) + 33;
-    if(!special){
-      if ((randomNumber >=33) && (randomNumber <=47)) { continue; }
-      if ((randomNumber >=58) && (randomNumber <=64)) { continue; }
-      if ((randomNumber >=91) && (randomNumber <=96)) { continue; }
-      if ((randomNumber >=123) && (randomNumber <=126)) { continue; }
-    }
+    randomNumber = Math.floor(Math.random()*10);;
     iteration++;
-    password += String.fromCharCode(randomNumber);
+    password += randomNumber.toString();
   }
   return password;
 }
