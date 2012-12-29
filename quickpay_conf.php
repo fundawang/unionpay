453bd25f59b327874b3f1500a5e844522bb5f891
Funda Wang <fundawang@gmail.com>
2012-12-30 06:13:50 +0800

    switch pre-online url

--- quickpay_conf.php
+++ quickpay_conf.php
@@ -28,13 +28,13 @@ class quickpay_conf
         'merAbbr'       => '商户名称',
     );
 
-    //* 测试环境
+    /* 测试环境
     static $front_pay_url   = "http://58.246.226.99/UpopWeb/api/Pay.action";
     static $back_pay_url    = "http://58.246.226.99/UpopWeb/api/BSPay.action";
     static $query_url       = "http://58.246.226.99/UpopWeb/api/Query.action";
     //*/
 
-    /* 预上线环境
+    //* 预上线环境
     static $front_pay_url   = "http://www.epay.lxdns.com/UpopWeb/api/Pay.action";
     static $back_pay_url    = "http://www.epay.lxdns.com/UpopWeb/api/BSPay.action";
     static $query_url       = "http://www.epay.lxdns.com/UpopWeb/api/Query.action";
