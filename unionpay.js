6a65f7052619fd5c0b9a58dcea1d84fcbc851d73
Funda Wang <fundawang@gmail.com>
2013-04-26 16:51:01 +0800

    set 32-digit password

--- unionpay.js
+++ unionpay.js
@@ -1,11 +1,10 @@
 function password(length) {
-  var iteration = 0;
-  var password = "";
-  var randomNumber;
-  while(iteration < length){
-    randomNumber = Math.floor(Math.random()*10);;
-    iteration++;
-    password += randomNumber.toString();
-  }
-  return password;
+	var iteration = 0;
+	var password = "";
+	var randomNumber;
+	var x="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	for(var i=0;i<length;i++)  {
+		password += x.charAt(Math.ceil(Math.random()*100000000)%x.length);
+	}
+	return password;
 }
